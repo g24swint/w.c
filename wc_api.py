5a8dfe7054652fddfd6777de0d70db8d85f6a766
Galen Swint <galen.swint@gmail.com>
2016-05-29 21:59:29 -0500

    refactor logging code

--- wc_api.py
+++ wc_api.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[9]:
+# In[14]:
 
 import sqlite3 as db
 import sqlalchemy as sa
@@ -9,27 +9,39 @@ import logging as logging
 import sys
 
 
-# In[10]:
-
-#fhandler = log.FileHandler(filename='mylog.log', mode='a')
-log = logging.getLogger("w_c")
-log_stream = logging.StreamHandler(sys.stdout)
-log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-log_stream.setFormatter(formatter)
-log.addHandler(log_stream)
-log.setLevel(logging.DEBUG)
-
-
-# In[12]:
-
-log.debug("Versions:")
-log.debug("  slqalchemy: {}".format(sa.__version__))
-log.debug("     sqlite3: {}".format(db.version))
-
-
 # In[ ]:
 
-sa.__version__
+# "global" variables
+wc_log = None
+
+
+# In[53]:
+
+def _init_logs(reset_handlers=True):
+    """Create a sys.stdout logger for w_c and print startup information 
+    
+    The logger name is the "w_c" namespace
+    """
+    global wc_log
+    log = logging.getLogger("w_c")
+    if reset_handlers:
+        log.handlers = []
+        log_stream = logging.StreamHandler(sys.stdout)
+        log_formatter = logging.Formatter('%(asctime)s::%(name)s::%(levelname)s::%(message)s')
+        log_stream.setFormatter(log_formatter)
+        log.addHandler(log_stream)
+        log.setLevel(logging.DEBUG)
+    log.debug("Versions:")
+    log.debug("  slqalchemy: {}".format(sa.__version__))
+    log.debug("     sqlite3: {}".format(db.version))
+    wc_log = log
+
+_init_logs()
+
+
+# In[50]:
+
+
 
 
 # In[7]:
